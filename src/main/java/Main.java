776b2b8f4687de79dade26f6e3aa55f5af3da551
서세훈 <ssa0100@naver.com>
2024-12-24 09:30:44 +0900

    work8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,7 +12,9 @@ class App {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
-        int lastNo = 1;
+        int lastNo = 0;
+        String content = "";
+        String author = "";
 
         while (true) {
             System.out.print("명령) ");
@@ -24,13 +26,17 @@ class App {
             } else if (command.equals("등록")) {
 
                 System.out.print("명언 : ");
-                scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
+                content = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
 
                 System.out.print("작가 : ");
-                scanner.nextLine();
+                author = scanner.nextLine();
 
-                System.out.println("%d 번 명언이 등록되었습니다.".formatted(lastNo));
+                System.out.println("%d 번 명언이 등록되었습니다.".formatted(++lastNo));
                 lastNo++;
+            } else if(command.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("--------------------");
+                System.out.println("$d / %s / %s".formatted(lastNo, author, content));
             }
         }
     }
