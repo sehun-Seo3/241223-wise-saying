27064ff58cfc1d34cb0126a5d9bc86f085c687ce
서세훈 <ssa0100@naver.com>
2024-12-24 09:43:56 +0900

    work9

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,29 +14,34 @@ class App {
         System.out.println("== 명언 앱 ==");
         int lastNo = 0;
         String content = "";
+        String[] contentList = new String[3];
         String author = "";
+        String[] authorList = new String[3];
 
         while (true) {
             System.out.print("명령) ");
             String command = scanner.nextLine();
 
-            if(command.equals("종료")){
+            if (command.equals("종료")) {
                 System.out.println("명언 앱을 종료합니다.");
                 break;
             } else if (command.equals("등록")) {
 
                 System.out.print("명언 : ");
-                content = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
+                contentList[lastNo] = scanner.nextLine(); // 입력값 가져옴. 입력값이 없으면 기다린다. 엔터를 쳐야 입력이 완료됨. 그래야 넘어감
 
                 System.out.print("작가 : ");
-                author = scanner.nextLine();
+                authorList[lastNo] = scanner.nextLine();
 
                 System.out.println("%d 번 명언이 등록되었습니다.".formatted(++lastNo));
                 lastNo++;
-            } else if(command.equals("목록")){
+            } else if (command.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언");
                 System.out.println("--------------------");
-                System.out.println("$d / %s / %s".formatted(lastNo, author, content));
+
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("$d / %s / %s".formatted(lastNo, authorList[i], contentList[i]));
+                }
             }
         }
     }
